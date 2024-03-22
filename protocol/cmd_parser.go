680265696456326b9012246ce25b83603ee4e55d
Om Jogani <om080jogani@gmail.com>
2024-03-22 10:54:08 +0530

    Add: Command Parser - Part of Protocol

---
+++ protocol/cmd_parser.go
@@ -0,0 +1,13 @@
+package protocol
+
+import "strings"
+
+func ParseCommand(cmdLine string) (cmd, param string) {
+	parts := strings.Split(cmdLine, " ")
+	if len(parts) != 2 {
+		return "", ""
+	}
+	cmd = strings.TrimSpace(parts[0])
+	param = strings.TrimSpace(parts[1])
+	return
+}
