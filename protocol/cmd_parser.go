29405550e060f97520a30585058b84235ce3a7cd
Om Jogani <om080jogani@gmail.com>
2024-04-03 14:26:01 +0530

    Fix: Command Parser for taking bodyType args

--- protocol/cmd_parser.go
+++ protocol/cmd_parser.go
@@ -7,18 +7,16 @@ import (
 
 func ParseCommand(fullCommand string) (command, path, bodyType string) {
 	breakdownCmd := strings.Split(fullCommand, " ")
+	bodyTypeVar := "NORMAL"
 	if len(breakdownCmd) < 2 {
 		log.Fatal("PARSER_ERROR: Please provide sufficient params to command!")
 		return "", "", ""
+	} else if len(breakdownCmd) >= 3 {
+		bodyTypeVar = strings.TrimSpace(breakdownCmd[2])
 	}
 
 	command = strings.TrimSpace(breakdownCmd[0])
 	path = strings.TrimSpace(breakdownCmd[1])
-	bodyType = strings.TrimSpace(breakdownCmd[2])
-
-	if bodyType == "" {
-		bodyType = "NORMAL"
-	}
-
+	bodyType = bodyTypeVar
 	return
 }
